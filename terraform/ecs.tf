5ba88e090295a703fd4b1354566d1afa1d8a2f9b
amitkumarnag <amit.nag89@gmail.com>
2025-05-12 10:00:46 +0200

    commit for infra and docker

---
+++ terraform/ecs.tf
@@ -0,0 +1,83 @@
+# This Terraform configuration sets up an AWS ECS cluster, task definition, and service.
+# It uses Fargate launch type and deploys a containerized application from an ECR repository.
+# The ECS service is configured to run one instance of the task definition.
+# It also includes network configuration for the service, specifying subnets and security groups.
+# The ECR repository is set to prevent destruction to avoid accidental data loss.
+resource "aws_ecs_cluster" "my_cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/my-container"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = var.ecs_task_family
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn           = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.container_name
+      image     = "${aws_ecr_repository.my_repository.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = var.ecs_service_name
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  desired_count   = 1
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = [var.security_group_id]
+    assign_public_ip = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,73 @@
+variable "aws_region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+variable "availability_zone" {
+  description = "The availability zone for the subnets"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+  default     = "my-app-repo"
+}
+
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  default     = "my-ecs-cluster"
+}
+
+variable "ecs_service_name" {
+  description = "The name of the ECS service"
+  type        = string
+  default     = "my-ecs-service"
+}
+
+variable "ecs_task_definition_name" {
+  description = "The name of the ECS task definition"
+  type        = string
+  default     = "my-ecs-task"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ECS service"
+  type        = list(string)
+  default     = ["subnet-01add066c918e7f25"]
+}
+
+variable "security_group_id" {
+  description = "The security group ID for the ECS service"
+  type        = string
+  default = "sg-019f60acd9c68e922"
+}
+
+variable "ecs_task_family" {
+  description = "The family name of the ECS task definition"
+  type        = string
+  default = "my-task-family"
+}
+
+variable "container_name" {
+  description = "The name of the container in the ECS task definition"
+  type        = string
+  default     = "my-container"
+}
